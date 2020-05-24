5e60dc82f3e2bf4268ee39f2f9e5b413c1103208
YizYah <yyakovson613@gmail.com>
2020-05-24 12:06:11 +0300

    generated no-stack code using make-code :tada:

--- src/components/RegistrationForm/index.js
+++ src/components/RegistrationForm/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Formik, Form } from 'formik';
-import { useMutation } from '@apollo/react-hooks';
-import * as Yup from 'yup';
-import { REGISTER_USER } from '@nostack/no-stack';
+import React, {useState} from 'react'
+import {Formik, Form} from 'formik'
+import {useMutation} from '@apollo/react-hooks'
+import * as Yup from 'yup'
+import {REGISTER_USER} from '@nostack/no-stack'
 
-import RegistrationField from './RegistrationField';
-import { Wrapper, Row, ErrorContainer } from './RegistrationForm.style';
+import RegistrationField from './RegistrationField'
+import {Wrapper, Row, ErrorContainer} from './RegistrationForm.style'
 
 const initialValues = {
   name: '',
@@ -14,52 +14,52 @@ const initialValues = {
   email: '',
   password: '',
   passwordConfirmation: '',
-};
+}
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
-    .label('name')
-    .required('Please enter desired username.'),
+  .label('name')
+  .required('Please enter desired username.'),
   firstName: Yup.string()
-    .label('firstName')
-    .required('Please enter your first name.'),
+  .label('firstName')
+  .required('Please enter your first name.'),
   lastName: Yup.string()
-    .label('lastName')
-    .required('Please enter your last name.'),
+  .label('lastName')
+  .required('Please enter your last name.'),
   email: Yup.string()
-    .label('email')
-    .email('Enter a valid email.')
-    .required('Please enter your email.'),
+  .label('email')
+  .email('Enter a valid email.')
+  .required('Please enter your email.'),
   password: Yup.string()
-    .label('password')
-    .matches(/(?=.*\d)/, 'Must have at least one numerical character')
-    .matches(
-      /(?=.*[#?!@$%^&*-.,:;'"><[\]{}()_|\\/~])/,
-      'Must have at least one special character.',
-    )
-    .min(8, 'Must be at least 8 characters.')
-    .required('Please enter your desired password.'),
+  .label('password')
+  .matches(/(?=.*\d)/, 'Must have at least one numerical character')
+  .matches(
+    /(?=.*[#?!@$%^&*-.,:;'"><[\]{}()_|\\/~])/,
+    'Must have at least one special character.',
+  )
+  .min(8, 'Must be at least 8 characters.')
+  .required('Please enter your desired password.'),
   passwordConfirmation: Yup.string()
-    .oneOf([Yup.ref('password'), '', null], 'Passwords must match.')
-    .required('Please confirm your password.'),
-});
+  .oneOf([Yup.ref('password'), '', null], 'Passwords must match.')
+  .required('Please confirm your password.'),
+})
 
 const RegistrationForm = ({
   userClassId,
   onSuccess,
 }) => {
-  const [register] = useMutation(REGISTER_USER);
-  const [registrationCompleted, setRegistrationCompleted] = useState(false);
-  const [formError, setFormError] = useState('');
+  const [register] = useMutation(REGISTER_USER)
+  const [registrationCompleted, setRegistrationCompleted] = useState(false)
+  const [formError, setFormError] = useState('')
 
   const handleSubmit = async (
     values,
-    { setSubmitting },
+    {setSubmitting},
   ) => {
-    setFormError('');
+    setFormError('')
 
     if (values.password !== values.passwordConfirmation) {
-      return;
+      return
     }
 
     try {
@@ -72,29 +72,29 @@ const RegistrationForm = ({
           email: values.email,
           password: values.password,
         },
-      });
+      })
 
-      setRegistrationCompleted(true);
+      setRegistrationCompleted(true)
 
       if (onSuccess) {
-        onSuccess();
+        onSuccess()
       }
     } catch (error) {
-      console.log(error);
-      console.log(error.graphQLErrors);
+      console.log(error)
+      console.log(error.graphQLErrors)
 
-      setFormError('Something went wrong. Please try again.');
+      setFormError('Something went wrong. Please try again.')
     }
 
-    setSubmitting(false);
-  };
+    setSubmitting(false)
+  }
 
   if (registrationCompleted) {
     return (
       <Wrapper>
-        <p>Successfully created account! You can now log in.</p>
+        <p>Please check your email and confirm that you are really you.  Then you can log in!</p>
       </Wrapper>
-    );
+    )
   }
 
   return (
@@ -104,7 +104,7 @@ const RegistrationForm = ({
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        {({ isSubmitting, isValid, dirty, isValidating }) => (
+        {({isSubmitting, isValid, dirty, isValidating}) => (
           <Form>
             <RegistrationField fieldLabel="Username:" type="text" name="name" />
             <RegistrationField
@@ -141,7 +141,7 @@ const RegistrationForm = ({
         )}
       </Formik>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default RegistrationForm;
+export default RegistrationForm
